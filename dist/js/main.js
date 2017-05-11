62a3b0df264189e4b82afd37d5efdffba3c203ae
newbienewbie <webbot@sina.com>
2017-05-11 15:22:52 +0800

    搭建游戏的基本模板

--- dist/js/main.js
+++ dist/js/main.js
@@ -71,6 +71,32 @@
 /***/ (function(module, exports) {
 
 
+class Game {
+
+    constructor(canvas, canvasContext, fps = 1000 / 60) {
+        this.canvas = canvas;
+        this.canvasContext = canvasContext;
+        this.fps = fps;
+    }
+
+    update() {}
+
+    draw() {
+        this.canvasContext.fillStyle = "deepskyblue";
+        this.canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+    }
+
+    run() {
+        this.update();
+        this.draw();
+        window.setTimeout(mainLoop, this.fps);
+    }
+}
+
+const canvas = document.getElementById("game");
+const canvasContext = canvas.getContext("2d");
+const game = new Game(canvas, canvasContext);
+game.run();
 
 /***/ })
 /******/ ]);
